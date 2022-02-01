b51d2499ba2b273faf22c4a2a4235e783870a152
Ilio Catallo <ilio.catallo@gmail.com>
2022-02-01 14:41:45 +0100

    Make `always()` narrow its output type

--- src/always.ts
+++ src/always.ts
@@ -1,5 +1,14 @@
 import { Type } from './Type'
 
-export function always<T>(value: T): Type<T, never> {
-  return new Type<T, never>((_: any) => value)
+export function always<T>(value: Narrow<T>): Type<T, never> {
+  return new Type<T, never>((_: any) => value as T)
 }
+
+type Narrow<A> =
+  | (A extends [] ? [] : never)
+  | (A extends Narrowable ? A : never)
+  | {
+      [K in keyof A]: A[K] extends Function ? A[K] : Narrow<A[K]>
+    }
+
+type Narrowable = string | number | bigint | boolean
