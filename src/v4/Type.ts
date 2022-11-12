6adc4671682b111de418be8639d8cfb752bdd738
Ilio Catallo <ilio.catallo@gmail.com>
2022-11-12 19:55:00 +0100

    Re-order production rules

--- src/v4/Type.ts
+++ src/v4/Type.ts
@@ -5,9 +5,10 @@ export interface Type<E> {
   to: Primitive | Match<E>
 }
 
+// ======================================================================
+// Primitive
+// ======================================================================
 export type Primitive = typeof PRIMITIVES[number]
-
-export type Match<E> = { match: E }
 
 export const string: Type<string> = { to: 'string' }
 
@@ -15,12 +16,17 @@ export const number: Type<number> = { to: 'number' }
 
 export const boolean: Type<boolean> = { to: 'boolean' }
 
-export function match<E extends string | number | boolean>(match: Narrow<E>): Type<Narrow<E>> {
-  return { to: { match } }
-}
-
 export function isPrimitive(to: any): to is Primitive {
   return PRIMITIVES.includes(to)
 }
 
 const PRIMITIVES = ['string', 'number', 'boolean'] as const
+
+// ======================================================================
+// Match
+// ======================================================================
+export type Match<E> = { match: E }
+
+export function match<E extends string | number | boolean>(match: Narrow<E>): Type<Narrow<E>> {
+  return { to: { match } }
+}
