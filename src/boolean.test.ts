12c16bf7d334bfae4bd55c71de666e9ba70f17a4
Ilio Catallo <ilio.catallo@gmail.com>
2022-12-07 15:23:07 +0100

    Type-check `coerceTo` return type as part of `boolean` test suite

--- src/boolean.test.ts
+++ src/boolean.test.ts
@@ -1,20 +1,35 @@
 import { test } from 'uvu'
-import { equal } from 'uvu/assert'
+import { is, equal } from 'uvu/assert'
 import fc, { assert, property } from 'fast-check'
 import { boolean } from './v4/Type'
 import { InvalidCoercion } from './InvalidCoercion'
 import { coerceTo } from './v4/coerceTo'
+import { expectType } from 'ts-expect'
 
 test(`boolean accepts boolean values`, function () {
-  assert(property(fc.boolean(), (value) => equal(coerceTo(boolean, value), value)))
+  assert(
+    property(fc.boolean(), (value) => {
+      const res = coerceTo(boolean, value)
+
+      expectType<boolean | InvalidCoercion>(res)
+      is(res, value)
+    })
+  )
 })
 
 test(`boolean rejects all but boolean values`, function () {
-  const notABoolean = fc.oneof(fcNumber, fc.string(), fc.constant(null), fc.constant(undefined), fcSymbol)
-  assert(property(notABoolean, (value) => equal(coerceTo(boolean, value), new InvalidCoercion('boolean', value))))
+  assert(
+    property(notABoolean, (value) => {
+      const res = coerceTo(boolean, value)
+
+      expectType<boolean | InvalidCoercion>(res)
+      equal(res, new InvalidCoercion('boolean', value))
+    })
+  )
 })
 
 test.run()
 
 const fcSymbol = fc.string().map((str) => Symbol(str))
 const fcNumber = fc.oneof(fc.integer(), fc.float(), fc.double())
+const notABoolean = fc.oneof(fcNumber, fc.string(), fc.constant(null), fc.constant(undefined), fcSymbol)
