70d469220ddfc8069e37a5ab73cbb5c667d98663
Ilio Catallo <ilio.catallo@gmail.com>
2022-12-07 15:30:14 +0100

    Type-check `coerceTo` return type as part of `number` test suite

--- src/number.test.ts
+++ src/number.test.ts
@@ -1,21 +1,35 @@
 import { test } from 'uvu'
-import { equal } from 'uvu/assert'
+import { is, equal } from 'uvu/assert'
 import fc, { assert, property } from 'fast-check'
 import { number } from './v4/Type'
 import { coerceTo } from './v4/coerceTo'
 import { InvalidCoercion } from './InvalidCoercion'
+import { expectType } from 'ts-expect'
 
 test(`number accepts number values`, function () {
-  assert(property(fcNumber, (value) => equal(coerceTo(number, value), value)))
+  assert(
+    property(fcNumber, (value) => {
+      const res = coerceTo(number, value)
+
+      expectType<number | InvalidCoercion>(res)
+      is(res, value)
+    })
+  )
 })
 
 test('number rejects all but number values', function () {
-  const notANumber = fc.oneof(fc.string(), fc.boolean(), fc.constant(null), fc.constant(undefined), fcSymbol)
+  assert(
+    property(notANumber, (value) => {
+      const res = coerceTo(number, value)
 
-  assert(property(notANumber, (value) => equal(coerceTo(number, value), new InvalidCoercion('number', value))))
+      expectType<number | InvalidCoercion>(res)
+      equal(res, new InvalidCoercion('number', value))
+    })
+  )
 })
 
 test.run()
 
 const fcSymbol = fc.string().map((str) => Symbol(str))
 const fcNumber = fc.oneof(fc.integer(), fc.float(), fc.double())
+const notANumber = fc.oneof(fc.string(), fc.boolean(), fc.constant(null), fc.constant(undefined), fcSymbol)
