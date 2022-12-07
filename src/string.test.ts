e59c5f6a1235b6a4d83f9bf1b87b3def31ff4a31
Ilio Catallo <ilio.catallo@gmail.com>
2022-12-07 15:33:24 +0100

    Type-check `coerceTo` return type as part of `string` test suite

--- src/string.test.ts
+++ src/string.test.ts
@@ -1,21 +1,35 @@
 import { test } from 'uvu'
-import { equal } from 'uvu/assert'
+import { is, equal } from 'uvu/assert'
 import fc, { assert, property } from 'fast-check'
 import { coerceTo } from './v4/coerceTo'
 import { string } from './v4/Type'
 import { InvalidCoercion } from './InvalidCoercion'
+import { expectType } from 'ts-expect'
 
 test(`string accepts string values`, function () {
-  assert(property(fc.string(), (value) => equal(coerceTo(string, value), value)))
+  assert(
+    property(fc.string(), (value) => {
+      const res = coerceTo(string, value)
+
+      expectType<string | InvalidCoercion>(res)
+      is(res, value)
+    })
+  )
 })
 
 test(`string rejects all but string values`, function () {
-  const notAString = fc.oneof(fcNumber, fc.boolean(), fc.constant(null), fc.constant(undefined), fcSymbol)
+  assert(
+    property(notAString, (value) => {
+      const res = coerceTo(string, value)
 
-  assert(property(notAString, (value) => equal(coerceTo(string, value), new InvalidCoercion('string', value))))
+      expectType<string | InvalidCoercion>(res)
+      equal(res, new InvalidCoercion('string', value))
+    })
+  )
 })
 
 test.run()
 
 const fcSymbol = fc.string().map((str) => Symbol(str))
 const fcNumber = fc.oneof(fc.integer(), fc.float(), fc.double())
+const notAString = fc.oneof(fcNumber, fc.boolean(), fc.constant(null), fc.constant(undefined), fcSymbol)
