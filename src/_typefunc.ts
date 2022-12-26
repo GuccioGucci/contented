50cc8e1482c9f398f5790771a30e82d75a31e5b7
Ilio Catallo <ilio.catallo@gmail.com>
2022-12-26 20:47:09 +0100

    Move `Narrow` in the `literal` module

--- src/_typefunc.ts
+++
@@ -1,6 +0,0 @@
-export type Narrow<A> =
-  | (A extends [] ? [] : never)
-  | (A extends string | number | boolean ? A : never)
-  | {
-      [K in keyof A]: A[K] extends Function ? A[K] : Narrow<A[K]>
-    }
--- src/literal.ts
+++ src/literal.ts
@@ -1,6 +1,12 @@
 import { Type } from './Type'
-import { Narrow } from './_typefunc'
 
 export function literal<R extends string | number | boolean>(value: Narrow<R>): Type<Narrow<R>> {
   return { schema: { literal: value } }
 }
+
+type Narrow<A> =
+  | (A extends [] ? [] : never)
+  | (A extends string | number | boolean ? A : never)
+  | {
+      [K in keyof A]: A[K] extends Function ? A[K] : Narrow<A[K]>
+    }
